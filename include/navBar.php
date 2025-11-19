e6a91920b1da507675cc71c86c811a75f295594a
NG ZI XUAN <zixuanng8368@gmail.com>
2025-11-19 17:36:12 +0800

    Updates

--- include/navBar.php
+++ include/navBar.php
@@ -1,22 +1,19 @@
 <nav class="navBar">
-        <div class="outer-container">
-            <a href="index.php" class="logo"><img src="img/logo.jpg" alt="FlavorHub's Logo" style="width: 15vw; max-height: 8vh;"></a>
-            <a href="recipe.php">Recipe</a>
-            <a href="ingredient.php">Ingredient</a>
-            <a href="cook.php">Cook</a>
-        </div>
+    <div class="outer-container">
+        <a href="index.php" class="logo"><img src="img/logo.jpg" alt="FlavorHub's Logo" style="width: 15vw; max-height: 8vh;"></a>
+        <a href="recipe.php">Recipe</a>
+        <a href="ingredient.php">Ingredient</a>
+        <a href="cook.php">Cook</a>
+    </div>
 
-        <div class="outer-container">
-            <?php 
-                if(!isset($_SESSION["username"]))
-                {
-                    echo    "<a href='register.php'>Register</a>
+    <div class="outer-container">
+        <?php
+        if (!isset($_SESSION["username"])) {
+            echo    "<a href='register.php'>Register</a>
                              <a href='login.php'>Login</a>";
-                }
-                else
-                {
-                    echo '<a href="#" onclick="window.location.href=\'logout.php\'">Logout</a>';
-                }
-            ?>
-        </div>
+        } else {
+            echo '<a href="#" onclick="window.location.href=\'logout.php\'">Logout</a>';
+        }
+        ?>
+    </div>
 </nav>
--- index.php
+++ index.php
@@ -18,8 +18,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/1801127a77.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style/style.css">
-    <link rel="stylesheet" href="style/navBar.css">
     <link rel="stylesheet" href="style/index.css">
+    <link rel="stylesheet" href="style/navBar.css">
     <title>FlavorHub</title>
 </head>
 <body>
@@ -30,9 +30,6 @@
         <div class="welcome-message">
             <h1>Welcome, <span><?= strtoupper($username) ?></span></h1>
             <p>This is the index page</p>
-            <!-- <div class="button-container">
-                <button class="<?= isset($_SESSION['username']) ? "active" : '' ?>"onclick="window.location.href='logout.php'">Logout</button>
-            </div> -->
         </div>
     </section>
     
